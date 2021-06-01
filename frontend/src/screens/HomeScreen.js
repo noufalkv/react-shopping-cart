fba08e3e273aee980c359843b563d41d9f16d351
noufal <noufalkv@gmail.com>
2021-06-01 15:12:23 +0400

    loaded product from backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Product from '../components/Product'
-import data from '../data';
+import LoadingBox from '../components/LoadingBox'
+import MessageBox from '../components/MessageBox'
+import axios from 'axios';
+// import data from '../data';
 
 export default function HomeScreen() {
+    const [products, setproducts] = useState([]);
+    const [loading, setloading] = useState(false);
+    const [error, setError] = useState(false);
+    useEffect(() => {
+      const fetchData = async () => {
+        try{
+          setloading(true);
+          const {data} = await axios.get('api/products');
+          setloading(false)
+          setproducts(data);
+        }catch(err)
+        {
+          setError(err.message);
+          setloading(false);
+
+        }
+       
+      };
+      fetchData();
+      
+    }, []);
     return (
-        <div>
-        <div className="row center">
-          {data.products.map((product) => (
-            <Product key={product._id} product={product}></Product>
-          ))}
-        </div>
+      <div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <>
+          {products.length === 0 && <MessageBox>No Product Found</MessageBox>}
+          <div className="row center">
+            {products.map((product) => (
+              <Product key={product._id} product={product}></Product>
+            ))}
+          </div>
+        </>
+      )}
       </div>
+        
+       
     )
 }
