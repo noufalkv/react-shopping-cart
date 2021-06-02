1bfcd531b429c2d5a2a8bd2f79d00f2548dd8782
noufal <noufalkv@gmail.com>
2021-06-02 15:20:47 +0400

    mongodb and backend user

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
 const data = {
+  users : [
+    {
+      name : "admin",
+      email : "noufalkv@gmail.com",
+      password: bcrypt.hashSync('1234',8),
+      isAdmin : true,
+    },
+    {
+      name : "Noufal",
+      email : "noufalkv1@gmail.com",
+      password: bcrypt.hashSync('1234',8),
+      isAdmin : true,
+    }
+  ],
     products: [
       {
         _id: '1',
